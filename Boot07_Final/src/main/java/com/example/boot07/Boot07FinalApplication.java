869502abcea6e1addcfbc6d44b524ad307e15420
yeonjinju <wnduswls21@gmail.com>
2023-07-28 12:59:15 +0900

    커밋

--- Boot07_Final/src/main/java/com/example/boot07/Boot07FinalApplication.java
+++ Boot07_Final/src/main/java/com/example/boot07/Boot07FinalApplication.java
@@ -13,13 +13,13 @@ public class Boot07FinalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Boot07FinalApplication.class, args);
 
-		// 크롬을 실행해서 http://localhost:9000/boot07 로딩하기
-		Runtime rt = Runtime.getRuntime();
-		try {
-			rt.exec("cmd /c start chrome.exe http://localhost:9000/boot07");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+//		// 크롬을 실행해서 http://localhost:9000/boot07 로딩하기
+//		Runtime rt = Runtime.getRuntime();
+//		try {
+//			rt.exec("cmd /c start chrome.exe http://localhost:9000/boot07");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 	}
 
 }
